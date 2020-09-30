1f0dcadd461c7daebc2396946620cf942938cec2
leanhtai01 <leanhtai01@gmail.com>
2020-09-30 17:26:02 +0700

    add project W2_RetrieveDataFromSQLServer

---
+++ W2_RetrieveDataFromSQLServer/FormStudent.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace W2_RetrieveDataFromSQLServer
+{
+    partial class FormStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxClass = new System.Windows.Forms.ComboBox();
+            this.dataGridViewStudent = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridViewStudent);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 86;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(800, 86);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.50871F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.49129F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxClass, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 23);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(140, 40);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lớp học:";
+            // 
+            // comboBoxClass
+            // 
+            this.comboBoxClass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxClass.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxClass.FormattingEnabled = true;
+            this.comboBoxClass.Location = new System.Drawing.Point(62, 9);
+            this.comboBoxClass.Name = "comboBoxClass";
+            this.comboBoxClass.Size = new System.Drawing.Size(75, 21);
+            this.comboBoxClass.TabIndex = 1;
+            this.comboBoxClass.SelectedValueChanged += new System.EventHandler(this.comboBoxClass_SelectedValueChanged);
+            // 
+            // dataGridViewStudent
+            // 
+            this.dataGridViewStudent.AllowUserToResizeRows = false;
+            this.dataGridViewStudent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewStudent.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewStudent.Name = "dataGridViewStudent";
+            this.dataGridViewStudent.ReadOnly = true;
+            this.dataGridViewStudent.RowHeadersVisible = false;
+            this.dataGridViewStudent.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewStudent.Size = new System.Drawing.Size(800, 360);
+            this.dataGridViewStudent.TabIndex = 0;
+            // 
+            // FormStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FormStudent";
+            this.Text = "Quản lý sinh viên";
+            this.Load += new System.EventHandler(this.FormStudent_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxClass;
+        private System.Windows.Forms.DataGridView dataGridViewStudent;
+    }
+}
+
---
+++ W2_RetrieveDataFromSQLServer/FormStudent.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W2_RetrieveDataFromSQLServer
+{
+    public partial class FormStudent : Form
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["SqlServerConnStr"].ConnectionString;
+        SqlConnection sqlConn;
+
+        public FormStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void FormStudent_Load(object sender, EventArgs e)
+        {
+            // declare variable
+            string command = "SELECT * FROM LopHoc;";
+
+            sqlConn = new SqlConnection(connStr);
+
+            comboBoxClass.DataSource = GetData(sqlConn, command);
+            comboBoxClass.DisplayMember = "TenLopHoc";
+            comboBoxClass.ValueMember = "LopHocID";
+
+            FillDataGridViewStudent(comboBoxClass.SelectedValue.ToString());
+        }
+
+        private void comboBoxClass_SelectedValueChanged(object sender, EventArgs e)
+        {
+            FillDataGridViewStudent(comboBoxClass.SelectedValue.ToString());
+        }
+
+        private void FillDataGridViewStudent(string id)
+        {
+            // declare variables
+            string command = "SELECT * FROM HocSinh WHERE LopHocID = @id;";
+            List<SqlParameter> parameters = new List<SqlParameter>();
+
+            // add parameter
+            parameters.Add(new SqlParameter("@id", id));
+
+            dataGridViewStudent.DataSource = GetData(sqlConn, command, parameters.ToArray());
+            dataGridViewStudent.Columns["HocSinhID"].HeaderText = "MSSV";
+            dataGridViewStudent.Columns["TenHocSinh"].HeaderText = "Tên học sinh";
+            dataGridViewStudent.Columns["NamSinh"].HeaderText = "Năm sinh";
+            dataGridViewStudent.Columns["DiemTrungBinh"].HeaderText = "ĐTB";
+            dataGridViewStudent.Columns["QueQuan"].HeaderText = "Quê quán";
+            dataGridViewStudent.Columns["LopHocID"].HeaderText = "Mã lớp học";
+            dataGridViewStudent.Columns["TenHocSinh"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewStudent.Columns["QueQuan"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        private DataTable GetData(SqlConnection sqlConn, string command, params SqlParameter[] parameters)
+        {
+            // declare variables
+            SqlDataAdapter sqlDataAdapter;
+            DataTable dataTable = new DataTable();
+            SqlCommand sqlCommand = new SqlCommand(command, sqlConn);
+
+            // add parameters to SqlCommand
+            sqlCommand.Parameters.AddRange(parameters);
+
+            // fill DataTable
+            sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+
+            return dataTable;
+        }
+    }
+}

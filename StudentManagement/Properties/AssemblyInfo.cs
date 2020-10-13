b491f2892dfd9309ba22463f0a00110611c004ea
leanhtai01 <leanhtai01@gmail.com>
2020-10-13 21:32:38 +0700

    change assembly information to StudentManagement

--- StudentManagement/Properties/AssemblyInfo.cs
+++ StudentManagement/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("W3-CRUDOnDatabases_FactoryDP")]
+[assembly: AssemblyTitle("StudentManagement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("W3-CRUDOnDatabases_FactoryDP")]
+[assembly: AssemblyProduct("StudentManagement")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

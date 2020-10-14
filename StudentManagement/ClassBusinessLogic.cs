5b5336bb9a8314379702b8a5b4eb62013c3d474a
leanhtai01 <leanhtai01@gmail.com>
2020-10-14 09:46:31 +0700

    add method GetClassList

--- StudentManagement/ClassBusinessLogic.cs
+++ StudentManagement/ClassBusinessLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,22 @@ namespace StudentManagement
 {
     public class ClassBusinessLogic
     {
+        private DataAccess dataAccess;
 
+        public ClassBusinessLogic()
+        {
+            dataAccess = new DataAccess();
+        }
+
+        /// <summary>
+        /// get class list
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetClassList()
+        {
+            string commandText = "SELECT * FROM LopHoc;";
+
+            return dataAccess.GetDataTable(commandText);
+        } // end method GetClassList
     }
 }

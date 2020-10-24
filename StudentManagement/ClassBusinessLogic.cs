0e5158e8d160b5bf78df56aaf7c8b5fd65d62f00
leanhtai01 <leanhtai01@gmail.com>
2020-10-24 17:37:59 +0700

    clean code

--- StudentManagement/ClassBusinessLogic.cs
+++ StudentManagement/ClassBusinessLogic.cs
@@ -12,33 +12,10 @@ namespace StudentManagement
     {
         private DataAccess dataAccess;
 
-        public ClassBusinessLogic()
-        {
-            dataAccess = new DataAccess();
-        }
-
         public ClassBusinessLogic(string providerName, string connectionStringName)
         {
             dataAccess = new DataAccess(providerName, connectionStringName);
         }
-
-        /// <summary>
-        /// get class list
-        /// </summary>
-        /// <returns></returns>
-        public DataTable GetClassList()
-        {
-            string commandText = "SELECT * FROM LopHoc;";
-
-            return dataAccess.GetDataTable(commandText);
-        } // end method GetClassList
-
-        public DataTable GetClassList(string tableName)
-        {
-            string commandText = "SELECT * FROM LopHoc;";
-
-            return dataAccess.GetDataTable(tableName, commandText);
-        } // end method GetClassList
 
         public DbDataAdapter GetClassList(DataSet dataSet, string tableName)
         {
@@ -46,104 +23,5 @@ namespace StudentManagement
 
             return dataAccess.GetDataTable(dataSet, tableName, commandText);
         } // end method GetClassList
-
-        public int UpdateClass(Class c)
-        {
-            string commandText =
-                "UPDATE LopHoc "
-                + "SET TenLopHoc = @TenLopHoc "
-                + "WHERE LopHocID = @LopHocId;";
-            DbParameter[] listParameters = new DbParameter[2];
-
-            // create parameters
-            for (int i = 0; i < listParameters.Length; i++)
-            {
-                listParameters[i] = dataAccess.Provider.CreateParameter();
-            }
-
-            // add parameters
-            listParameters[0].ParameterName = "@LopHocID";
-            listParameters[0].Value = c.Id;
-            listParameters[1].ParameterName = "@TenLopHoc";
-            listParameters[1].Value = c.Name;
-
-            return dataAccess.ExecuteNonQuery(commandText, listParameters);
-        }
-
-        public Class GetClass(string classId)
-        {
-            string commandText = "SELECT * FROM LopHoc WHERE LopHocID = @classId;";
-            DbParameter[] listParameters = new DbParameter[1];
-            Class c = new Class();
-            DbDataReader reader;
-
-            listParameters[0] = dataAccess.Provider.CreateParameter();
-            listParameters[0].ParameterName = "@classId";
-            listParameters[0].Value = classId;
-
-            reader = dataAccess.ExecuteReader(commandText, listParameters);
-
-            while (reader.Read())
-            {
-                c.Id = reader["LopHocID"].ToString();
-                c.Name = reader["TenLopHoc"].ToString();
-            }
-
-            dataAccess.Connection.Close();
-
-            return c;
-        }
-
-        public int InsertClass(Class c)
-        {
-            string commandText =
-                "INSERT INTO LopHoc (LopHocID, TenLopHoc) "
-                + "VALUES (@Id, @Name);";
-            DbParameter[] listParameters = new DbParameter[2];
-
-            // create parameters
-            for (int i = 0; i < listParameters.Length; i++)
-            {
-                listParameters[i] = dataAccess.Provider.CreateParameter();
-            }
-
-            // add parameters
-            listParameters[0].ParameterName = "@Id";
-            listParameters[0].Value = c.Id;
-            listParameters[1].ParameterName = "@Name";
-            listParameters[1].Value = c.Name;
-
-            return dataAccess.ExecuteNonQuery(commandText, listParameters);
-        }
-
-        public bool IsClassExists(string classId)
-        {
-            string commandText = "SELECT 1 FROM LopHoc WHERE LopHocId = @classId;";
-            DbParameter[] listParameters = new DbParameter[1];
-
-            // create parameters
-            listParameters[0] = dataAccess.Provider.CreateParameter();
-
-            // add parameters
-            listParameters[0].ParameterName = "@classId";
-            listParameters[0].Value = classId;
-
-            return dataAccess.ExecuteScalar(commandText, listParameters) != null;
-        }
-
-        public int DeleteClass(string classId)
-        {
-            string commandText = "DELETE FROM LopHoc WHERE LopHocID = @classId";
-            DbParameter[] listParameters = new DbParameter[1];
-
-            // create parameters
-            listParameters[0] = dataAccess.Provider.CreateParameter();
-
-            // add parameters
-            listParameters[0].ParameterName = "@classId";
-            listParameters[0].Value = classId;
-
-            return dataAccess.ExecuteNonQuery(commandText, listParameters);
-        }
     }
 }
--- StudentManagement/DataAccess.cs
+++ StudentManagement/DataAccess.cs
@@ -12,65 +12,12 @@ namespace StudentManagement
         public DbProviderFactory Provider { get; set; }
         public DbConnection Connection { get; set; }
 
-        public DataAccess()
-        {
-            Provider = DbProviderFactories.GetFactory(providerName);
-            Connection = Provider.CreateConnection();
-            Connection.ConnectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
-        }
-
         public DataAccess(string providerName, string connectionStringName)
         {
             Provider = DbProviderFactories.GetFactory(providerName);
             Connection = Provider.CreateConnection();
             Connection.ConnectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
         }
-
-        /// <summary>
-        /// get data from database
-        /// </summary>
-        /// <param name="commandText"></param>
-        /// <param name="parameters"></param>
-        /// <returns></returns>
-        public DataTable GetDataTable(string commandText, params DbParameter[] parameters)
-        {
-            // declare variables
-            DbDataAdapter dataAdapter = Provider.CreateDataAdapter();
-            DataTable dataTable = new DataTable();
-            DbCommand command = Provider.CreateCommand();
-
-            // add parameters to DbCommand
-            command.Connection = Connection;
-            command.CommandText = commandText;
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddRange(parameters);
-
-            // fill DataTable
-            dataAdapter.SelectCommand = command;
-            dataAdapter.Fill(dataTable);
-
-            return dataTable;
-        } // end method GetData
-
-        public DataTable GetDataTable(string tableName, string commandText, params DbParameter[] parameters)
-        {
-            // declare variables
-            DbDataAdapter dataAdapter = Provider.CreateDataAdapter();
-            DataTable dataTable = new DataTable(tableName);
-            DbCommand command = Provider.CreateCommand();
-
-            // add parameters to DbCommand
-            command.Connection = Connection;
-            command.CommandText = commandText;
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddRange(parameters);
-
-            // fill DataTable
-            dataAdapter.SelectCommand = command;
-            dataAdapter.Fill(dataTable);
-
-            return dataTable;
-        } // end method GetData
 
         public DbDataAdapter GetDataTable(DataSet dataSet, string tableName, string commandText, params DbParameter[] parameters)
         {
@@ -94,58 +41,5 @@ namespace StudentManagement
 
             return dataAdapter;
         } // end method GetData
-
-        public object ExecuteScalar(string commandText, params DbParameter[] dbParameters)
-        {
-            DbCommand command = Provider.CreateCommand();
-            object obj = null;
-
-            // add parameters to DbCommand
-            command.Connection = Connection;
-            command.CommandText = commandText;
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddRange(dbParameters);
-
-            Connection.Open();
-            obj = command.ExecuteScalar();
-            Connection.Close();
-
-            return obj;
-        } // end method ExecuteScalar
-
-        public int ExecuteNonQuery(string commandText, DbParameter[] dbParameters)
-        {
-            DbCommand command = Provider.CreateCommand();
-            int numOfRowsAffected;
-
-            // add parameters to DbCommand
-            command.Connection = Connection;
-            command.CommandText = commandText;
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddRange(dbParameters);
-
-            Connection.Open();
-            numOfRowsAffected = command.ExecuteNonQuery();
-            Connection.Close();
-
-            return numOfRowsAffected;
-        } // end method ExecuteNonQuery
-
-        public DbDataReader ExecuteReader(string commandText, DbParameter[] dbParameters)
-        {
-            DbCommand command = Provider.CreateCommand();
-            DbDataReader reader;
-
-            // add parameters to DbCommand
-            command.Connection = Connection;
-            command.CommandText = commandText;
-            command.CommandType = CommandType.Text;
-            command.Parameters.AddRange(dbParameters);
-
-            Connection.Open();
-            reader = command.ExecuteReader();
-
-            return reader;
-        }
     }
 }
